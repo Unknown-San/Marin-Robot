4bf79d43bddd01af4f6a4d66ba6fee68a395e52a
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-01-28 12:23:19 -0800

    Update functions.py

--- MarinRobot/utils/functions.py
+++ MarinRobot/utils/functions.py
@@ -32,9 +32,9 @@ import speedtest
 from PIL import Image, ImageDraw, ImageFilter, ImageFont
 from pyrogram.types import Message
 
-from Zaid import aiohttpsession as aiosession
-from Zaid.ex_plugins.dbfunctions import start_restart_stage
-from Zaid.utils.http import get, post
+from MarinRobot import aiohttpsession as aiosession
+from MarinRobot.ex_plugins.dbfunctions import start_restart_stage
+from MarinRobot.utils.http import get, post
 
 
 async def restart(m: Message):
