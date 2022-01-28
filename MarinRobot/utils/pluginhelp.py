355a6f57664bfd117a5b59bb7057e7ec493b24bb
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-01-28 12:08:43 -0800

    Update pluginhelp.py

--- MarinRobot/utils/pluginhelp.py
+++ MarinRobot/utils/pluginhelp.py
@@ -328,6 +328,15 @@ def capture_err(func):
     return capture
 
 
+async def fetch(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            try:
+                data = await resp.json()
+            except Exception:
+                data = await resp.text()
+    return data
+
 # Special credits to TheHamkerCat
 
 
