b2009318837cb30516f0b2065a67bd8c45070fcf
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-01-28 18:44:11 -0800

    Update mongo.py

--- MarinRobot/mongo.py
+++ MarinRobot/mongo.py
@@ -3,19 +3,19 @@ import sys
 from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
-from Zaid import MONGO_DB_URI
+from MarinRobot import MONGO_DB_URI
 
 
 MONGO_PORT = "27017"
 MONGO_DB_URI = "mongodb+srv://Hirokun:hirokun_5@cluster0.kprej.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-MONGO_DB = "ZaidRobot"
+MONGO_DB = "MarinRobot"
 
 
 client = MongoClient()
 client = MongoClient(MONGO_DB_URI, 27017)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, 27017)
 db = motor[MONGO_DB]
-db = client["ZaidRobot"]
+db = client["MarinRobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
 except ServerSelectionTimeoutError:
