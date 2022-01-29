5c96f75a40ee174623661206cc9a9053a17651f5
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-01-28 18:39:46 -0800

    Update http.py

--- MarinRobot/utils/http.py
+++ MarinRobot/utils/http.py
@@ -19,7 +19,7 @@ SOFTWARE.
 """
 from asyncio import gather
 
-from Zaid import aiohttpsession as session
+from MarinRobot import aiohttpsession as session
 
 
 async def get(url: str, *args, **kwargs):
