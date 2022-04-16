c746c091edcbaea39bbdce81784630b7b3cd8b37
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2022-04-16 09:39:41 +0530

    Create Cat.py

---
+++ MarinRobot/modules/Cat.py
@@ -0,0 +1,29 @@
+from pyrogram import filters
+
+from MarinRobot import pbot as app
+from MarinRobot.utils.errors import capture_err
+
+
+
+
+@app.on_message(filters.command("cat"))
+@capture_err
+async def take_ss(_, message):
+    try:
+        if len(message.command) != 2:
+            await message.reply_text("Give A Query Meow!.")
+            return
+        url = message.text.split(None, 1)[1]
+        m = await message.reply_text("Taking Screenshot")
+        await m.edit("Uploading")
+        try:
+            await app.send_photo(
+                message.chat.id,
+                photo=f"https://cataas.com/cat",
+            )
+        except TypeError:
+            await m.edit("No Response Meow!.")
+            return
+        await m.delete()
+    except Exception as e:
+        await message.reply_text(str(e))
